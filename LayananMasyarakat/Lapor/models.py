4d268a4de864c9e03e8222efc1dfbc22ddafd06f
Bimo Prasetyo Afif <bimo.prasetyoafif@gmail.com>
2019-12-01 17:45:34 +0700

    add relational model

--- LayananMasyarakat/Lapor/models.py
+++ LayananMasyarakat/Lapor/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 
-# Create your models here.
-class Laporan(models.Model):
-    judul = models.CharField(max_length=100)
-    deskripsi = models.CharField(max_length=5000)
-    status = models.CharField(max_length=25)
-    kategori = models.CharField(max_length=50)
-    datetime = models.DateTimeField()
-
 class Komentar(models.Model):
-    isi = models.CharField(max_length=500)
-    datetime = models.DateTimeField()
+    isi         = models.CharField(max_length=500)
+    datetime    = models.DateTimeField()
 
 class Vote(models.Model):
     pilihan = models.CharField(max_length=100)
@@ -21,8 +13,20 @@ class District(models.Model):
 class Kategori(models.Model):
     nama = models.CharField(max_length=50)
 
+# Create your models here.
+class Laporan(models.Model):
+    judul       = models.CharField(max_length=100)
+    deskripsi   = models.CharField(max_length=5000)
+    status      = models.CharField(max_length=25)
+    kategori    = models.CharField(max_length=50)
+    komentar    = models.ManyToManyField(Komentar)
+    vote        = models.ManyToManyField(Vote)
+    district    = models.ForeignKey(District, on_delete=models.CASCADE)
+    kategori    = models.ForeignKey(Kategori, on_delete=models.CASCADE)
+    datetime    = models.DateTimeField()
+
 class Custom_user(models.Model):
-    nik = models.CharField(max_length=20)
-    daerah = models.CharField(max_length=25)
+    nik     = models.CharField(max_length=20)
+    daerah  = models.CharField(max_length=25)
     telepon = models.CharField(max_length=15)
     jabatan = models.CharField(max_length=20)
